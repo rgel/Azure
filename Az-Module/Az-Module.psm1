ca2b6f52c43fcf9ae7b135dc95a921f88be8ee37
Roman Gelman <rgel75@gmail.com>
2016-06-29 12:39:34 +0300

    Update Az-Module.psm1

--- Az-Module/Az-Module.psm1
+++ Az-Module/Az-Module.psm1
@@ -59,3 +59,172 @@ Filter Get-AzVmTag {
 	}
 
 } #End Filter Get-AzVmTag
+
+Function Add-AzVmTag {
+
+<#
+.SYNOPSIS
+	Add Resource Tags for Azure VMs.
+.DESCRIPTION
+	This cmdlet adds/sets Resource Tag/Tags for Azure VMs.
+.PARAMETER VM
+	Azure Virtual Machines.
+.PARAMETER Tag
+	VM Tag name.
+.PARAMETER Value
+	VM Tag value.
+.PARAMETER Force
+	Overwrite tag if exists.
+.PARAMETER TagPowerOn
+	VM 'PowerOn' tag's value.
+.PARAMETER TagPowerOff
+	VM 'PowerOff' tag's value.
+.EXAMPLE
+	PS C:\> $MyAzureResourceGroup = 'Production'
+	PS C:\> Get-AzureRmVM -ResourceGroupName $MyAzureResourceGroup |sort Name |Add-AzVmTag |ft -au
+	Set default 'PowerOn' and 'PowerOff' tag values to all VM in a Resource Group.
+.EXAMPLE
+	PS C:\> Get-AzureRmVM -Name 'azvm1' -ResourceGroupName $MyAzureResourceGroup |Add-AzVmTag -TagPowerOn '08:00' -TagPowerOff '21:00'
+	Set custom 'PowerOn' and 'PowerOff' tag values to a single VM.
+.EXAMPLE
+	PS C:\> Get-AzureRmVM -ResourceGroupName $MyAzureResourceGroup |? {$_.Name -like 'azvm*'} |Add-AzVmTag -Tag Environment -Value Prod -Force
+	Set custom tag to filtered out VMs, overwrite the tag if exists.
+.INPUTS
+	[Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine[]] AzureRm VM objects, returned by Get-AzureRmVM cmdlet.
+.OUTPUTS
+	[System.Management.Automation.PSCustomObject] PSObject collection.
+.NOTES
+	Author       ::	Roman Gelman.
+	Dependencies ::	AzureRm PowerShell Module.
+	Version 1.0  ::	27-Jun-2016  :: Release.
+.LINK
+	https://goo.gl/vAxH2a
+#>
+
+[CmdletBinding(DefaultParameterSetName='POWER')]
+
+Param (
+
+	[Parameter(Mandatory,Position=1,ValueFromPipeline)]
+		[Alias("AzureVm")]
+	[Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine[]]$VM
+	,
+	[Parameter(Mandatory=$false,Position=2,ParameterSetName='POWER')]
+		[ValidateNotNullorEmpty()]
+	[datetime]$TagPowerOn = '06:00'
+	,
+	[Parameter(Mandatory=$false,Position=3,ParameterSetName='POWER')]
+		[ValidateNotNullorEmpty()]
+	[datetime]$TagPowerOff = '18:00'
+	,
+	[Parameter(Mandatory,Position=2,ParameterSetName='CUSTOM')]
+		[Alias("TagName")]
+		[ValidateSet("PowerOn","PowerOff","Environment","Project","Notes")]
+	[string]$Tag
+	,
+	[Parameter(Mandatory,Position=3,ParameterSetName='CUSTOM')]
+		[Alias("TagValue")]
+	[string]$Value
+	,
+	[Parameter(Mandatory=$false,Position=4,ParameterSetName='CUSTOM')]
+		[Alias("Overwrite")]
+	[switch]$Force
+)
+
+Begin {
+
+	$ErrorActionPreference = 'Stop'
+	
+	If ($PSCmdlet.ParameterSetName -eq 'POWER') {
+		$TimeFormat   = 'HH:mm'
+		$PowerOnTime  = $TagPowerOn.ToString($TimeFormat)
+		$PowerOffTime = $TagPowerOff.ToString($TimeFormat)
+	}
+	
+} #End Begin
+
+Process {
+
+	### Power Tags ###
+	If ($PSCmdlet.ParameterSetName -eq 'POWER') {
+			
+		Foreach ($AzVm in $VM) {
+		
+			 Try
+				{
+					If ($AzVm.Tags.Keys -contains 'poweron')  {$null = $AzVm.Tags.Remove('PowerOn')}
+					If ($AzVm.Tags.Keys -contains 'poweroff') {$null = $AzVm.Tags.Remove('PowerOff')}
+					
+					$null = $AzVm.Tags.Add('PowerOn',$PowerOnTime)
+					$null = $AzVm.Tags.Add('PowerOff',$PowerOffTime)
+				
+					$JobPower = Update-AzureRmVM -VM $AzVm -ResourceGroupName $AzVm.ResourceGroupName
+					$Properties = [ordered]@{
+						AzureVm       = $AzVm.Name
+						ResourceGroup = $AzVm.ResourceGroupName
+						PowerOn       = $PowerOnTime
+						PowerOff      = $PowerOffTime
+						StatusCode    = $JobPower.StatusCode
+					}
+				}
+		   Catch
+				{
+					$Properties = [ordered]@{
+						AzureVm       = $AzVm.Name
+						ResourceGroup = $AzVm.ResourceGroupName
+						PowerOn       = $PowerOnTime
+						PowerOff      = $PowerOffTime
+						StatusCode    = 'Error'
+					}
+				}
+		 Finally
+				{
+					$Object = New-Object PSObject -Property $Properties
+					$Object
+				}
+		} #End Foreach
+	### Custom Tag ###	
+	} Else {
+		
+		Foreach ($AzVm in $VM) {
+		
+			 Try
+				{
+					If ($AzVm.Tags.Keys -contains $Tag) {
+						If ($Force) {$null = $AzVm.Tags.Remove($Tag)}
+						Else {$Status = 'TagExists'}
+					}
+					If ($Status -ne 'TagExists') {
+						$AzVm.Tags.Add($Tag,$Value)
+						$JobCustom = Update-AzureRmVM -VM $AzVm -ResourceGroupName $AzVm.ResourceGroupName
+						$Status = $JobCustom.StatusCode
+					}
+					$Properties = [ordered]@{
+						AzureVm       = $AzVm.Name
+						ResourceGroup = $AzVm.ResourceGroupName
+						Tag           = $Tag
+						Value         = $Value
+						StatusCode    = $Status
+					}
+				}
+		   Catch
+				{
+					$Properties = [ordered]@{
+						AzureVm       = $AzVm.Name
+						ResourceGroup = $AzVm.ResourceGroupName
+						Tag           = $Tag
+						Value         = $Value
+						StatusCode    = 'Error'
+					}
+				}
+		 Finally
+				{
+					$Object = New-Object PSObject -Property $Properties
+					$Object
+				}
+		} #End Foreach
+	}
+	
+} #End Process
+
+} #End Function Add-AzVmTag
